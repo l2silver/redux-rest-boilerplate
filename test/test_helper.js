f86d087b5f72a204ab3810ffd10604cdd26a2a26
l2silver <lsilversteinto@gmail.com>
2016-01-18 21:32:56 -0500

    add test helper

---
+++ test/test_helper.js
@@ -0,0 +1,19 @@
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+import chaiAsPromised from 'chai-as-promised';
+
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
+
+chai.use(chaiImmutable);
+chai.use(chaiAsPromised);
+
